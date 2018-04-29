1430f7725d1f188faacccb1cff37ae13e70dd52b
Ryodan <YZN2130624@INF.JU.EDU.JO>
2018-04-29 19:07:14 +0300

    fixed index page displaying only one craftsmen bug

--- includes/dbHandler/indexHandler.php
+++ includes/dbHandler/indexHandler.php
@@ -14,7 +14,7 @@ if(isset($_GET['action'])){
     switch($action){
 
         case 'topCraftsmen' :{
-            $stmt = "SELECT first_name,last_name,SUM(rate) as rate,count(rate_id) as numofrates,username,city,craft,mobile FROM masteruser inner JOIN craftsmen on craftsmen_id = user_id inner join craftsmen_mobile on craftsmen.craftsmen_id = craftsmen_mobile.craftsmen_id left join rate on craftsmen.craftsmen_id=rate.craftsmen_id GROUP BY rate.craftsmen_id order by rate DESC limit 8 
+            $stmt = "SELECT first_name,last_name,SUM(rate) as rate,count(rate_id) as numofrates,username,city,craft,mobile FROM masteruser inner JOIN craftsmen on craftsmen_id = user_id inner join craftsmen_mobile on craftsmen.craftsmen_id = craftsmen_mobile.craftsmen_id left join rate on craftsmen.craftsmen_id=rate.craftsmen_id GROUP BY craftsmen.craftsmen_id order by rate DESC limit 8 
                     ";
 
             $query = $db->prepare($stmt);
