b96e3bae6e675ba2ae675936932f62d6d7809ada
amr0139840 <AMR0139840@INF.JU.EDU.JO>
2018-05-02 22:48:33 +0300

    fix some bugs

--- profile-includes/header.php
+++ profile-includes/header.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
         <nav class="navbar navbar-default navbar-static-top m-b-0">
             <div class="navbar-header"> <a class="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i class="fa fa-bars"></i></a>
-                <div class="top-left-part"><a href="index.php"><img src="images/logo.png" width="155px" height="40px" alt="Home"></a></div>
+                <div style="float:left;"><a href="index.php"><img src="images/logo.png" width="155px" height="40px" alt="Home"></a></div>
                
                 <ul class="nav navbar-top-links navbar-right pull-right">
                     <li>
